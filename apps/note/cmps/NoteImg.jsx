72b3245229adafa090359002a1c27ed7cbba13d6
seanErmilov <seanermilov321@gmail.com>
2024-06-28 16:25:13 +0300

    Improve desighn of list notes

--- apps/note/cmps/NoteImg.jsx
+++ apps/note/cmps/NoteImg.jsx
@@ -2,7 +2,7 @@ export function NoteImg(props) {
   const { info } = props
   const { title, imgUrl } = info
   return (
-    <div className='note-txt'>
+    <div className='note-img'>
       <div className='note-title'>{title}</div>
       <img src={imgUrl} alt='' />
     </div>
