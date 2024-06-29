d5f930433885921cfcac5199317afa43ce2a4cbf
Sagiteichman <chittosin@gmail.com>
2024-06-29 14:39:18 +0300

    added about css with our pictures and details

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,7 +1,22 @@
 export function About() {
   return (
-    <section className='about'>
-      <h1>About Page let call it test my friend</h1>
-    </section>
+    <section className="about-us">
+    <h1>About Us</h1>
+    <div className="team">
+        <div className="team-member">
+            <img src="../../../assets/img/about/sagi.png" alt="Sagi Teichman"/>
+            <h2>Sagi Teichman</h2>
+            <p>Founder & Email Developer</p>
+            <p>With a passion for technology and innovation, Sagi Teichman leads the development of Email App, ensuring the app delivers top-notch functionality and user experience.</p>
+        </div>
+        <div className="team-member">
+            <img src="../../../assets/img/about/sean.png" alt="Sean Ermilov"/>
+            <h2>Sean Ermilov</h2>
+            <p>Founder & Note Designer</p>
+            <p>Sean Ermilov brings a keen eye for design and a deep understanding of user needs, crafting intuitive and beautiful interfaces that make AppSus a joy to use.</p>
+        </div>
+    </div>
+</section>
+
   )
 }
